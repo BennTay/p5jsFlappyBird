e2f327bd2aecf360e004d549d546fd8cd2267713
BennTay <benntayguobin@gmail.com>
2019-07-19 12:09:19 +0800

    Fix bird file extension

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ var score = 0,
   game_started = false;
 
 function preload() {
-  birdImg = loadImage('images/bird2.png');
+  birdImg = loadImage('images/bird2.jpg');
   bird.imgSprite = birdImg;
 }
 
