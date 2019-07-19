fea0b394dbd2b3c69d7d40363476d6efdfa42a0c
BennTay <benntayguobin@gmail.com>
2019-07-19 12:10:43 +0800

    Try bird3

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ var score = 0,
   game_started = false;
 
 function preload() {
-  birdImg = loadImage('images/bird2.jpg');
+  birdImg = loadImage('images/bird3.png');
   bird.imgSprite = birdImg;
 }
 
