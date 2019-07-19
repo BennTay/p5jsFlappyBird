dc31f74ed7cafb1bf2cbaf582155d345ce42732d
BennTay <benntayguobin@gmail.com>
2019-07-19 12:12:33 +0800

    Revert to bird 1

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ var score = 0,
   game_started = false;
 
 function preload() {
-  birdImg = loadImage('images/bird3.png');
+  birdImg = loadImage('images/bird1.png');
   bird.imgSprite = birdImg;
 }
 
